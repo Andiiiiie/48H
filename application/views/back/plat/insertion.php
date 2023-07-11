93901c63466e539b93fd582f7a83ffac3360dfab
Andiiiiie <andiejessica2020@gmail.com>
2023-07-11 15:30:48 +0300

    crud-1

--- application/views/back/plat/insertion.php
+++ application/views/back/plat/insertion.php
@@ -2,60 +2,46 @@
 <div class="right_col" role="main">
     <!-- top tiles -->
     <div class="row">
-        <div class="x_panel">
-            <div class="x_title">
-                <h2>Insertion Plat</h2>
-                <ul class="nav navbar-right panel_toolbox">
-                    <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
-                    </li>
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
-                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                            <a class="dropdown-item" href="#">Settings 1</a>
-                            <a class="dropdown-item" href="#">Settings 2</a>
-                        </div>
-                    </li>
-                    <li><a class="close-link"><i class="fa fa-close"></i></a>
-                    </li>
-                </ul>
-                <div class="clearfix"></div>
-            </div>
-            <div class="x_content">
 
-                <!-- start form for validation -->
-                <form id="demo-form" data-parsley-validate>
-                    <div class="form-group">
-                        <label for="designation">Designation * :</label>
-                        <input type="text" id="designation" class="form-control" name="designation" required />
-                        <span class="text-danger"><?= get_error($errors, 'designation') ?></span>
-                    </div>
-
-                    <div class="form-group">
-                        <label for="image_path">Photo :</label>
-                        <input class="date-picker form-control col-md-7 col-xs-12" type="file" name="image_path"
-                               value="<?= set_value('designation') ?>">
-                        <span class="text-danger"><?= get_error($errors, 'image_path') ?></span>
-                    </div>
-
-                    <div class="form-group">
-                        <label for="ingredient[]">Ingredients:</label>
-                        <p style="padding: 5px;">
-                            <input type="checkbox" name="hobbies[]" id="hobby2" value="run" class="flat" /> Running
-                            <br />
-                            <input type="checkbox" name="hobbies[]" id="hobby3" value="eat" class="flat" /> Eating
-                            <br />
-                            <input type="checkbox" name="hobbies[]" id="hobby4" value="sleep" class="flat" /> Sleeping
-                            <br />
-                        </p>
-                    </div>
-
-
+        <div class="col-md-12 col-sm-12 col-xs-12">
+            <div class="x_panel">
+                <div class="x_title">
+                    <h2>Insertion Plat</h2>
+                    <div class="clearfix"></div>
+                </div>
+                <div class="x_content">
                     <br />
-                    <span class="btn btn-primary">enregistrer plat</span>
-                </form>
-                <!-- end form for validations -->
+                    <form id="demo-form2" class="form-horizontal form-label-left" method="post" enctype="multipart/form-data">
+                        <div class="form-group">
+                            <label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-name">Désignation <span class="required">*</span>
+                            </label>
+                            <div class="col-md-6 col-sm-6 col-xs-12">
+                                <input type="text" class="form-control col-md-7 col-xs-12" name="designation"
+                                       value="<?= set_value('designation') ?>">
+                                <span class="text-danger"><?= get_error($errors, 'designation') ?></span>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="control-label col-md-3 col-sm-3 col-xs-12">Photo d'illustration du plat <span class="required">*</span>
+                            </label>
+                            <div class="col-md-6 col-sm-6 col-xs-12">
+                                <input class="date-picker form-control col-md-7 col-xs-12" type="file" name="image_path"
+                                       value="<?= set_value('image_path') ?>">
+                                <span class="text-danger"><?= get_error($errors, 'image_path') ?></span>
+                            </div>
+                        </div>
+                        <div class="ln_solid"></div>
+                        <div class="form-group">
+                            <div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">
+                                <a class="btn btn-primary" href="<?= site_url('back/sport/liste') ?>">Retourner à la liste</a>
+                                <button type="submit" class="btn btn-success">Insérer composition</button>
+                            </div>
+                        </div>
 
+                    </form>
+                </div>
             </div>
         </div>
+
     </div>
 </div>
--- application/views/back/sport/modifier.php
+++ application/views/back/sport/modifier.php
@@ -6,29 +6,23 @@
         <div class="col-md-12 col-sm-12 col-xs-12">
             <div class="x_panel">
                 <div class="x_title">
-                    <h2>Modification d'une composition</h2>
+                    <h2>Insertion d'une activite sportive</h2>
                     <div class="clearfix"></div>
                 </div>
                 <div class="x_content">
                     <br />
                     <form id="demo-form2" class="form-horizontal form-label-left" method="post" enctype="multipart/form-data">
                         <div class="form-group">
-                            <label class="control-label col-md-3 col-sm-3 col-xs-12">Photo de description actuelle</label>
-                            <div class="col-md-6 col-sm-6 col-xs-12">
-                                <img src="<?= base_url($sport['image_path']) ?>" alt="Photo de description actuelle" class="img-responsive">
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label class="control-label col-md-3 col-sm-3 col-xs-12">Désignation <span class="required">*</span>
+                            <label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-name">Désignation <span class="required">*</span>
                             </label>
                             <div class="col-md-6 col-sm-6 col-xs-12">
                                 <input type="text" class="form-control col-md-7 col-xs-12" name="designation"
-                                       value="<?= set_value('composition', $sport['designation']) ?>">
+                                       value="<?= set_value('designation') ?>">
                                 <span class="text-danger"><?= get_error($errors, 'designation') ?></span>
                             </div>
                         </div>
                         <div class="form-group">
-                            <label class="control-label col-md-3 col-sm-3 col-xs-12">Photo de description <span class="required">*</span>
+                            <label class="control-label col-md-3 col-sm-3 col-xs-12">Photo d'illustration de l'activite <span class="required">*</span>
                             </label>
                             <div class="col-md-6 col-sm-6 col-xs-12">
                                 <input class="date-picker form-control col-md-7 col-xs-12" type="file" name="image_path"
@@ -40,7 +34,7 @@
                         <div class="form-group">
                             <div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">
                                 <a class="btn btn-primary" href="<?= site_url('back/sport/liste') ?>">Retourner à la liste</a>
-                                <button type="submit" class="btn btn-success">Modifier composition</button>
+                                <button type="submit" class="btn btn-success">Insérer composition</button>
                             </div>
                         </div>
 
